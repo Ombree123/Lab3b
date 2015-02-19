e751760443fb1926236b1098be385655363cc3e6
unknown <1338238@INFO-C3003-04.pedagogie.cegep-chicoutimi.qc.ca>
2015-02-19 11:25:49 -0500

    Projet vide

---
+++ Lab3b/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3b
+{
+    class Article
+    {
+        private string nom; 
+        private float valeur;
+
+        public static void ajouterNomArticle (string n)
+       {
+        
+
+        }
+        public static void ajouterValeurArticle(float v)
+        {
+        }
+
+        public static float lireValeurArticle()
+        {
+            
+        }
+
+        public static string lireDescriptionArticle()
+        {
+
+        }
+
+
+}
+}
---
+++ Lab3b/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Article article = new Article();
+
+
+
+
+
+        }
+    }
+}
